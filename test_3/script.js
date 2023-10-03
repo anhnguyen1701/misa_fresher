62abea2dee214ffa9bb4d236057b3043726a712e
anh nguyen <nguyenlamanh1701@gmail.com>
2023-10-03 17:07:53 +0700

    ud

--- test_3/script.js
+++ test_3/script.js
@@ -6,10 +6,14 @@ const comboboxData = document.getElementById('combobox_data');
 const comboboxItems = comboboxData.querySelectorAll('li');
 
 window.onload = function () {
+    console.log(comboboxData);
     document.addEventListener('click', toggleComboboxData);
     comboboxItems.forEach((item) => {
         item.addEventListener('click', passValueToInput);
     })
+    comboboxInput.addEventListener('click', filter);
+    comboboxBtn.addEventListener('click', filter)
+    comboboxInput.addEventListener('keyup', filter);
 }
 
 // show and hide combobox data
@@ -29,18 +33,18 @@ function toggleComboboxData(event) {
     }
 }
 
+//pass valuet to input and reset css
 function passValueToInput() {
-    if (this.value == -1) {
+    if (this.value == -1)
         comboboxInput.value = '';
-    } else {
-        comboboxInput.value = this.innerHTML;
-    }
+    else
+        comboboxInput.value = this.querySelector('.combobox__data-li-text').innerText;
 
     //delete
     try {
         let comboboxItemCheck = comboboxData.querySelector('.combobox__data-li-check');
         comboboxItemCheck.classList.remove('combobox__data-li-check');
-        comboboxItemCheck.removeChild(comboboxItemCheck.querySelector('span'));
+        comboboxItemCheck.removeChild(comboboxItemCheck.querySelector('.icon'));
     } catch (error) {
         console.log(error);
     }
@@ -51,5 +55,20 @@ function passValueToInput() {
     this.classList.add('combobox__data-li-check');
 }
 
+//search
+function filter() {
+    let keyword = comboboxInput.value.toLowerCase();
+    let list = comboboxData.querySelectorAll('.combobox__data-li-text');
+
+    list.forEach((item) => {
+        if (item.innerText.toLowerCase().indexOf(keyword) > -1) {
+            item.parentNode.style.display = '';
+        } else {
+            item.parentNode.style.display = "none";
+        }
+    })
+
+    comboboxData.querySelector('li[value="-1"]').style.display = '';
+}
 
 
