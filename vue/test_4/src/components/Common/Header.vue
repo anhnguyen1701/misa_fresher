9027b010c7c19233de78cc914f6b6a390fad6c20
anh nguyen <nguyenlamanh1701@gmail.com>
2023-10-05 12:49:28 +0700

    ud folder structure

---
+++ vue/test_4/src/components/Common/Header.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div>
+    <header>
+      <div class="header__left">
+        <div class="header__sidebar">
+          <span class="material-symbols-outlined"> apps </span>
+        </div>
+        <div class="header__logo"></div>
+      </div>
+      <div class="header__right">
+        <div class="header__account">
+          <div class="header__avatar"></div>
+          <div class="header__username">Nguyễn Lâm Anh</div>
+          <div class="header__option">
+            <span class="material-symbols-outlined"> more_horiz </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
