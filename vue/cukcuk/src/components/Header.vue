f02c26b2e62d7f6880e81966d623ebf2966dcf66
anh nguyen <nguyenlamanh1701@gmail.com>
2023-10-07 21:50:41 +0700

    header

--- vue/cukcuk/src/components/Header.vue
+++ vue/cukcuk/src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
   <header>
-    <h1>header</h1>
+    <div class="left">
+      <div class="icon-left"></div>
+      <div class="logo"></div>
+    </div>
+    <div class="right">
+      <div class="avatar"></div>
+      <span class="name">Nguyễn Lâm Anh</span>
+      <div class="icon-right"></div>
+    </div>
   </header>
 </template>
 
@@ -13,8 +21,69 @@ export default {
 
 <style scoped>
 header {
-  /* width: 100%; */
-  background: red;
+  background: white;
   height: 56px;
+  font-size: 14px;
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left {
+  margin-left: 16px;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  gap: 16px;
+}
+
+.right {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  gap: 24px;
+  margin-right: 16px;
+}
+
+.icon-left {
+  background-image: url('../assets/img/toggle.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 24px;
+  height: 24px;
+  line-height: 56px;
+  display: block;
+  line-height: 56px;
+  border-radius: 50%;
+}
+
+.logo {
+  background-image: url('../assets/img/cukcuk-logo.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  height: calc(175px / 4);
+  width: 175px;
+}
+
+.avatar {
+  background-image: url('../assets/img/default-avatar.jpg');
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  height: 32px;
+  width: 32px;
+  border-radius: 50%;
+}
+
+.icon-right {
+  background-image: url('../assets/icon/option.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  height: 24px;
+  width: 24px;
 }
 </style>
